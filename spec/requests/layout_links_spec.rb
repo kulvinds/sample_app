68ffcc35026eb75b0366aef943b13807602c61cd
Kulvinder Singh <kulvinds@thoughtworks.com>
2012-02-24 15:05:26 +0530

    Finishedd first cut of the user model

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+  before(:each) do
+     @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -25,5 +29,20 @@ describe "LayoutLinks" do
   it "should have a signup page at '/signup'" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
   end
 end 
